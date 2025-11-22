ab4adcd127ae19c968795d0b4102315a026e4777
jduja <jduja681@gmail.com>
2025-11-22 15:55:57 +0800

    aewe

--- TileMosaic/TileMosaic/SceneDelegate.swift
+++ TileMosaic/TileMosaic/SceneDelegate.swift
@@ -49,6 +49,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            ATTrackingManager.requestTrackingAuthorization {_ in }
+        }
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
